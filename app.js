61142c0b06177d5b56681fef659633fd98da1194
Scott Tippetts <wstippetts+github@gmail.com>
2023-01-23 16:07:10 -0700

    week2 day1 start

---
+++ app.js
@@ -0,0 +1,23 @@
+let score1 = 0
+let score2 = 0
+
+
+let but1 = []
+let but2 = []
+
+function redTeamClick() {
+  but1.push(1);
+  total;
+}
+
+function wizTeamClick() {
+  but2.push(1);
+  total;
+}
+
+function total() {
+  score1 = but1.length
+  score2 = but2.length
+}
+
+console.log("mad team", score1, "wizards", score2);
